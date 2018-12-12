ad2168c0bd7c03465aadf2d971ef443465d586cb
2472114784@qq.com <2472114784@qq.com>
2018-12-12 09:09:04 +0800

    dd

---
+++ vue_elem/src/pages/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <v-content></v-content>
+    <v-footer></v-footer>
+  </div>
+</template>
+<script>
+import content from '../components/Home_Content'
+import footer from '../components/Home_Footer'
+
+export default {
+  data() {
+    return {
+      message: "hello vue!"
+    };
+  },
+
+  methods: {
+    
+  },
+  components:{
+    'v-content':content,
+    'v-footer':footer
+  }
+};
+</script>
+<style scoped>
